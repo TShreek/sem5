abd948f8f4c257ca1f1b4f3be1d78ce41c36749f
Shreekrishna T <124446964+TShreek@users.noreply.github.com>
2025-02-03 10:33:52 +0530

    Update p11.java

--- java/p11/p11.java
+++ java/p11/p11.java
@@ -1,18 +1,22 @@
-import java.util.*;
 import java.io.*;
-import jakarta.servlet.x;
-import jakarta.servlet.http.*;
-public class ValidateAgeServlet extends HttpServlet{
-    protected void doPost(HttpServletRequest request, HttpServletResponse response throws ServletException, IDException){
-        String name = request.getParameter("name");
-        int age = Integer.parseInt(request.getParameter("age"));
+import javax.servlet.*;
+import javax.servlet.http.*;
 
+public class CheckAge extends HttpServlet{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+        String name = request.getParameter("name");
+        int age = request.parseInt(request.getParameter("age"));
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+
         out.println("<html><body>");
-        (age > 18)? out.println("<h1>Welcome " + name + "</h1>") : out.println("<h1>Sorry " + name + ", you are not eligible to vote.</h1>");
+        if(age <18){
+            out.println("<p>SORRY " + name + " YOU ARE NOT AUTHORIZED TO VIEW THIS PAGE<p>");
+        }
+        else{
+            out.println("<p>Welcome to the website "+ name + "!!<p>");
+        }
         out.println("</body></html>");
         out.close();
     }
-
 }
